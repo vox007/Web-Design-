e89e6c0d7f43259208e3666369d54ac0b4cd7a40
vox007 <44740807+vox007@users.noreply.github.com>
2018-12-14 21:19:21 +0700

    Create NavB.A.r

---
+++ NavB.A.r
@@ -0,0 +1,32 @@
+ <!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Unless i stuck with you</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+  </head>
+  <body>
+    <nav class="navbar navbar-default navbar-inverse navbar-fixed-top">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <a href="#" class="navbar-brand">Company</a>
+
+        </div>
+        <ul class="nav navbar-nav">
+          <li><a href="#">Item One</a></li>
+          <li><a href="#">Item Two</a></li>
+
+        </ul>
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="#">Sunflower</a></li>
+
+        </ul>
+
+
+      </div>
+
+
+    </nav>
+
+  </body>
+</html>
